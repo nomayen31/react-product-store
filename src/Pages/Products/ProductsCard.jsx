5ad8ae9b7cd67479433a58e7cc43a00a5add019f
nomayen <nomayen.ohin@gmail.com>
2023-10-26 14:01:00 +0600

    daynamic card crerate

--- src/Pages/Products/ProductsCard.jsx
+++ src/Pages/Products/ProductsCard.jsx
@@ -1,18 +1,20 @@
 
 
-const ProductsCard = (props) => {
-    const { name } = props;
+const ProductsCard = ({product}) => {
+    console.log(product);
+    const {name, price, image, description } = product;
     return (
         <>
         <div>
            
            <div className="card w-96 bg-base-100 shadow-xl">
                <figure className="px-10 pt-10">
-                   <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                   <img src={image} alt="Shoes" className="rounded-xl w-[270px] h-[200px]" />
                </figure>
                <div className="card-body items-center text-center">
                    <h2 className="card-title">{name}</h2>
-                   <p>If a dog chews shoes whose shoes does he choose?</p>
+                   <p>{description}</p>
+                   <p>{price}</p>
                    <div className="card-actions">
                        <button className="btn btn-primary">Buy Now</button>
                    </div>
